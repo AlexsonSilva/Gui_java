50e6a480bdeca624db370a8d11e99d3fd86e9a47
alexsonsilva <21643@student.dorset-college.ie>
2021-03-30 16:03:17 +0100

    Third commit

--- src/com/company/alexson/MainTTT.java
+++ src/com/company/alexson/MainTTT.java
@@ -1,17 +1,16 @@
-package com.company;
+package com.company.alexson;
 
 import javax.swing.*;
-import java.awt.*;
 
-public class Main {
+public class MainTTT {
 
     public static void main(String[] args) {
 
-//        Login login = new Login(); //creates a frame
-//        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
-//        login.setResizable(false); //prevent frame from being resized
-//        login.setSize(660, 440); //sets the x-dimension, and y-dimension of frame
-//        login.setVisible(true); //make frame visible
+        Login login = new Login(); //creates a frame
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
+        login.setResizable(false); //prevent frame from being resized
+        login.setSize(660, 440); //sets the x-dimension, and y-dimension of frame
+        login.setVisible(true); //make frame visible
 
         StudentPage studentPage = new StudentPage(); //creates a frame
         studentPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
--- src/com/company/alexson/StudentRegistration.java
+++ src/com/company/alexson/StudentRegistration.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.alexson;
 
 import javax.swing.*;
 import java.awt.*;
